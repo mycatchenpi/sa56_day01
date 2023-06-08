4e37277c8c27a9cf48fb6c9ba025963adfd2d228
susie <chenpitools@gmail>
2023-06-08 15:01:37 +0800

    test01

---
+++ src/main/java/sg/edu/nus/iss/s56_day01/S56Day01Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.s56_day01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S56Day01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S56Day01Application.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/s56_day01/S56Day01ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.s56_day01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S56Day01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
